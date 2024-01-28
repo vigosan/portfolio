ffd93ee1fbefd6386f4f7d032b6897534d8c610d
Vicent Gozalbes <vigosan@gmail.com>
2024-01-28 19:59:22 +0100

    install prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  proseWrap: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+};
