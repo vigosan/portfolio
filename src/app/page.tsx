0e4cfb67f84a20c0072d645268203b9cf5df6a3b
Vicent Gozalbes <vigosan@gmail.com>
2024-01-29 12:24:12 +0100

    fix icon and text align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,26 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <section className='flex flex-col gap-10'>
+    <section className="flex flex-col gap-10">
       <h1 className="text-2xl font-medium tracking-tighter">
         hello, I&apos;m vicent 👋
       </h1>
       <p>
         I&apos;m a full-stack developer with a specific passion for the
         frontend. I currently work at{' '}
-        <a href="http://flywire.com" className="whitespace-nowrap">
+        <a
+          href="http://flywire.com"
+          className="group inline-flex items-center align-middle"
+        >
           <Image
             alt="Flywire icon"
             aria-hidden
-            className="inline-block h-4 w-4 text-red-500"
+            className="h-auto w-4"
             src="/fly.svg"
             height={16}
             width={16}
           />
-          <span className="inline-block">Flywire</span>
+          <span>Flywire</span>
         </a>
         , contributing to several projects built in React. Follow my journey as
         I continue to explore the realms of code and innovation!
