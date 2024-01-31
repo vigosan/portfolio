7946db283214de5fa8334c9921846e8f1ad2f767
Vicent Gozalbes <vigosan@gmail.com>
2024-01-31 19:41:32 +0100

    add hover classes to link

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer>
       <a
         href="mailto:hola@vicent.io"
-        className="text-md block text-right leading-5"
+        className="text-md block text-right leading-5 hover:underline hover:decoration-blue-500 hover:decoration-wavy dark:hover:decoration-slate-500/50"
       >
         hola@vicent.io
       </a>
