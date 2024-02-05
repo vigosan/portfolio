589cdb5709a9b51f60a87827f80b2d33934e6ab2
Vicent Gozalbes <vigosan@gmail.com>
2024-02-05 19:56:53 +0100

    remove experimental load for scripts

--- ui/Analytics.tsx
+++ ui/Analytics.tsx
@@ -2,9 +2,6 @@ import Script from 'next/script';
 
 export default function Analytics() {
   return (
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=G-11197KZXNE"
-      strategy="worker"
-    />
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-11197KZXNE" />
   );
 }
